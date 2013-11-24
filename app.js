66a159e326cfed8edef66fd22552d4fa570e1129
aslinwang <dreamjet.wang@gmail.com>
2013-11-24 16:39:05 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-var app = require('express').createServer();
+var express = require('express'),
+	http = require('http');
+
+var app = express();
+
+app.configure(function(){
+	app.set('port', 80);
+	app.set('view engine', 'ejs');
+	app.set('views', __dirname + '/views');
+
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.use(express.methodOverride());
+	app.use(express.static(__dirname + 'public'));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler());
+})
+
 app.get('/', function(req, res) {
+	console.log(req);
     res.send('Hello World');
 });
-app.listen(process.env.VCAP_APP_PORT || 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('listening on port ' + app.get('port'));
+});
