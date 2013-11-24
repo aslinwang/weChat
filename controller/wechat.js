4235e6765e09e9c7e17772030112ecd594602cf4
aslinwang <dreamjet.wang@gmail.com>
2013-11-24 10:48:53 +0000

    add wechat auth check logic

--- controller/wechat.js
+++ controller/wechat.js
@@ -1,13 +1,34 @@
 "use strict"
-var url = require('url');
 
-function checkSignature(){
-	
+var CONFIG = require(../config);
+
+var url = require('url'),
+    crypto = require('crypto');
+
+function checkSignature(ts, nonce, sign){
+    var token = CONFIG.TOKEN,
+	arr = [token, ts, nonce];
+    
+    var res = arr.sort().join();
+    var sha1 = crypto.createHash('sha1');
+
+    sha1.update(res);
+    if(sha1.digest('hex') == sign){
+        return true;
+    }
+    else{
+	return false;
+    }
 }
 
 exports.index = function(req, res){
-	var _url = url.parse(req.url);
+	var _url = url.parse(req.url, true);
 	var _params = _url.query;
-
-	console.log(_params);
+	
+	if(checkSignature(_params['timestamp'], _params['nonce'], _param['signature'])){
+	    res.send(_params['echostr']);
+	}
+	else{
+	    res.send('ok');
+	}
 }
