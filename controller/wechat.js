eb7f872765958ba8fea078817cb812b7112f3d3b
aslinwang <dreamjet.wang@gmail.com>
2013-11-24 11:12:46 +0000

    update wechat.js

--- controller/wechat.js
+++ controller/wechat.js
@@ -1,6 +1,6 @@
 "use strict"
 
-var CONFIG = require(../config);
+var CONFIG = require('../config');
 
 var url = require('url'),
     crypto = require('crypto');
@@ -9,10 +9,11 @@ function checkSignature(ts, nonce, sign){
     var token = CONFIG.TOKEN,
 	arr = [token, ts, nonce];
     
-    var res = arr.sort().join();
+    var res = arr.sort().join('');
     var sha1 = crypto.createHash('sha1');
 
     sha1.update(res);
+    console.log(res);
     if(sha1.digest('hex') == sign){
         return true;
     }
@@ -25,7 +26,7 @@ exports.index = function(req, res){
 	var _url = url.parse(req.url, true);
 	var _params = _url.query;
 	
-	if(checkSignature(_params['timestamp'], _params['nonce'], _param['signature'])){
+	if(checkSignature(_params['timestamp'], _params['nonce'], _params['signature'])){
 	    res.send(_params['echostr']);
 	}
 	else{
